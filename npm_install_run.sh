a9e3f1c5c32c1500265c38031388a03e9598daa1
Manojkumar Movva <movvamanoj@gmail.com>
2024-01-12 11:30:39 +0530

    Update npm_install_run.sh

--- npm_install_run.sh
+++ npm_install_run.sh
@@ -1,37 +1,42 @@
-
 #!/bin/bash
+# # Change to the home directory
+# cd ~
 
-install_common_packages() {
-  echo "Installing common packages..."
+# # Download app.js from GitHub to the home path
+# curl -O https://raw.githubusercontent.com/movvamanoj/static-webhost/main/app.js
+
+# # Display the path where app.js is downloaded
+# downloaded_file_path=$(pwd)/app.js
+# echo "app.js is downloaded to: $downloaded_file_path"
+
+# Function to check npm version
+check_npm_version() {
+  if ! command -v npm &> /dev/null; then
+    echo "npm is not installed. Installing npm..."
+    install_npm_based_on_os
+  fi
+  echo "npm version: $(npm --version)"
 }
 
-install_wget_if_needed() {
-  if ! command -v wget &>/dev/null; then
-    echo "Installing wget..."
-    # Use the appropriate package manager for your distribution
-    if [[ -f /etc/redhat-release ]]; then
-      sudo yum install -y wget
-    elif [[ -f /etc/lsb-release ]]; then
-      sudo apt-get update
-      sudo apt-get install -y wget
-    # Add more distribution-specific checks as needed
-    else
-      echo "Unsupported distribution. Please install wget manually."
-      exit 1
-    fi
+install_npm_based_on_os() {
+  # Check the Linux distribution and run commands accordingly
+  if [[ -f /etc/redhat-release ]]; then
+    configure_nodejs_npm_redhat
+  elif [[ -f /etc/lsb-release ]]; then
+    configure_nodejs_npm_ubuntu
+  elif [[ -f /etc/system-release-cpe ]]; then
+    configure_nodejs_npm_amazon
+  elif [[ -f /etc/centos-release ]]; then
+    configure_nodejs_npm_centos
+  else
+    echo "Unsupported operating system. Please install npm manually."
+    exit 1
   fi
 }
 
-# # Specify the desired directory path
-#   target_directory="~/"
-  
-#   # Use wget with -O option to download and save in the specified directory
-#   wget https://raw.githubusercontent.com/movvamanoj/static-webhost/main/app.js -O "$target_directory/app.js"
-
-
-download_appjs() {
-  echo "Downloading app.js from GitHub..."
-  wget https://raw.githubusercontent.com/movvamanoj/static-webhost/main/app.js -O app.js
+install_common_packages() {
+  echo "Installing common packages..."
+  # Add commands for installing common packages if needed
 }
 
 configure_nodejs_npm_redhat() {
@@ -40,7 +45,7 @@ configure_nodejs_npm_redhat() {
   mkdir ~/.npm-global
   npm config set prefix '~/.npm-global'
   export PATH=~/.npm-global/bin:$PATH
-  sudo npm install -g npm@10.2.5
+  sudo npm install -g npm@10.3.0
 }
 
 configure_nodejs_npm_ubuntu() {
@@ -49,7 +54,7 @@ configure_nodejs_npm_ubuntu() {
   mkdir ~/.npm-global
   npm config set prefix '~/.npm-global'
   export PATH=~/.npm-global/bin:$PATH
-  sudo npm install -g npm@10.2.5
+  sudo npm install -g npm@10.3.0
 }
 
 configure_nodejs_npm_amazon() {
@@ -58,7 +63,7 @@ configure_nodejs_npm_amazon() {
   mkdir ~/.npm-global
   npm config set prefix '~/.npm-global'
   export PATH=~/.npm-global/bin:$PATH
-  sudo npm install -g npm@10.2.5
+  sudo npm install -g npm@10.3.0
 }
 
 configure_nodejs_npm_centos() {
@@ -68,10 +73,11 @@ configure_nodejs_npm_centos() {
   mkdir ~/.npm-global
   npm config set prefix '~/.npm-global'
   export PATH=~/.npm-global/bin:$PATH
-  sudo npm install -g npm@10.2.5
+  sudo npm install -g npm@10.3.0
 }
 
 install_nodejs_packages() {
+  check_npm_version
   echo "Installing Node.js packages..."
   npm install express body-parser aws-sdk
   npm install -g pm2
@@ -105,296 +111,37 @@ install_configure_cloudwatch_agent_centos() {
   sudo systemctl start amazon-cloudwatch-agent
   sudo systemctl enable amazon-cloudwatch-agent
 }
-start_pm2() {
-  echo "Making app.js executable..."
-  sudo chmod +x app.js
 
-  echo "Starting app with pm2..."
-  pm2 start app.js
-}
+# Check npm version before proceeding
+check_npm_version
 
-# Check for wget and install if needed
-install_wget_if_needed
+install_common_packages
 
 # Check the Linux distribution and run commands accordingly
 if [[ -f /etc/redhat-release ]]; then
-  install_common_packages
-  download_appjs
   configure_nodejs_npm_redhat
-  npm install   # Run npm install before starting pm2
   install_nodejs_packages
   install_configure_cloudwatch_agent_redhat
 elif [[ -f /etc/lsb-release ]]; then
-  install_common_packages
-  download_appjs
   configure_nodejs_npm_ubuntu
-  npm install   # Run npm install before starting pm2
   install_nodejs_packages
   install_configure_cloudwatch_agent_ubuntu
-elif [[ -f /etc/system-release && $(cat /etc/system-release) == "Amazon Linux" ]]; then
+elif [[ -f /etc/system-release-cpe ]]; then
   configure_nodejs_npm_amazon
-  npm install   # Run npm install before starting pm2
   install_nodejs_packages
   install_configure_cloudwatch_agent_amazon
 elif [[ -f /etc/centos-release ]]; then
-  install_common_packages
-  download_appjs
   configure_nodejs_npm_centos
-  npm install   # Run npm install before starting pm2
   install_nodejs_packages
   install_configure_cloudwatch_agent_centos
 fi
 
-start_pm2
-# #!/bin/bash
-
-# install_common_packages() {
-#   echo "Installing common packages..."
-# }
-
-# install_wget_if_needed() {
-#   if ! command -v wget &>/dev/null; then
-#     echo "Installing wget..."
-#     # Use the appropriate package manager for your distribution
-#     if [[ -f /etc/redhat-release ]]; then
-#       sudo yum install -y wget
-#     elif [[ -f /etc/lsb-release ]]; then
-#       sudo apt-get update
-#       sudo apt-get install -y wget
-#     # Add more distribution-specific checks as needed
-#     else
-#       echo "Unsupported distribution. Please install wget manually."
-#       exit 1
-#     fi
-#   fi
-# }
-
-# # # Specify the desired directory path
-# #   target_directory="~/"
-  
-# #   # Use wget with -O option to download and save in the specified directory
-# #   wget https://raw.githubusercontent.com/movvamanoj/static-webhost/main/app.js -O "$target_directory/app.js"
-
-
-# download_appjs() {
-#   echo "Downloading app.js from GitHub..."
-#   wget https://raw.githubusercontent.com/movvamanoj/static-webhost/main/app.js -O app.js
-# }
-
-# configure_nodejs_npm_redhat() {
-#   echo "Configuring Node.js and npm for Red Hat..."
-#   sudo yum install -y nodejs npm
-#   mkdir ~/.npm-global
-#   npm config set prefix '~/.npm-global'
-#   export PATH=~/.npm-global/bin:$PATH
-#   sudo npm install -g npm@10.2.5
-# }
-
-# configure_nodejs_npm_ubuntu() {
-#   echo "Configuring Node.js and npm for Ubuntu..."
-#   sudo apt-get update && sudo apt-get install -y nodejs npm
-#   mkdir ~/.npm-global
-#   npm config set prefix '~/.npm-global'
-#   export PATH=~/.npm-global/bin:$PATH
-#   sudo npm install -g npm@10.2.5
-# }
-
-# configure_nodejs_npm_amazon() {
-#   echo "Configuring Node.js and npm for Amazon Linux..."
-#   sudo yum install -y nodejs npm
-#   mkdir ~/.npm-global
-#   npm config set prefix '~/.npm-global'
-#   export PATH=~/.npm-global/bin:$PATH
-#   sudo npm install -g npm@10.2.5
-# }
-
-# configure_nodejs_npm_centos() {
-#   echo "Configuring Node.js and npm for CentOS..."
-#   sudo yum install -y epel-release
-#   sudo yum install -y nodejs npm
-#   mkdir ~/.npm-global
-#   npm config set prefix '~/.npm-global'
-#   export PATH=~/.npm-global/bin:$PATH
-#   sudo npm install -g npm@10.2.5
-# }
-
-# install_nodejs_packages() {
-#   echo "Installing Node.js packages..."
-#   npm install express body-parser aws-sdk
-#   npm install -g pm2
-#   npm install winston winston-cloudwatch @aws-sdk/client-dynamodb
-# }
-
-# install_configure_cloudwatch_agent_redhat() {
-#   echo "Installing and configuring Amazon CloudWatch Agent for Red Hat..."
-#   sudo yum install -y amazon-cloudwatch-agent
-#   sudo systemctl start amazon-cloudwatch-agent
-#   sudo systemctl enable amazon-cloudwatch-agent
-# }
-
-# install_configure_cloudwatch_agent_ubuntu() {
-#   echo "Installing and configuring Amazon CloudWatch Agent for Ubuntu..."
-#   sudo apt-get install -y amazon-cloudwatch-agent
-#   sudo systemctl start amazon-cloudwatch-agent
-#   sudo systemctl enable amazon-cloudwatch-agent
-# }
-
-# install_configure_cloudwatch_agent_amazon() {
-#   echo "Installing and configuring Amazon CloudWatch Agent for Amazon Linux..."
-#   sudo yum install -y amazon-cloudwatch-agent
-#   sudo systemctl start amazon-cloudwatch-agent
-#   sudo systemctl enable amazon-cloudwatch-agent
-# }
-
-# install_configure_cloudwatch_agent_centos() {
-#   echo "Installing and configuring Amazon CloudWatch Agent for CentOS..."
-#   sudo yum install -y amazon-cloudwatch-agent
-#   sudo systemctl start amazon-cloudwatch-agent
-#   sudo systemctl enable amazon-cloudwatch-agent
-# }
-
-# start_pm2() {
-#   echo "Starting app with pm2..."
-#   pm2 start app.js
-# }
-
-# # Check for wget and install if needed
-# install_wget_if_needed
-
-# # Check the Linux distribution and run commands accordingly
-# if [[ -f /etc/redhat-release ]]; then
-#   install_common_packages
-#   download_appjs
-#   configure_nodejs_npm_redhat
-#   install_nodejs_packages
-#   install_configure_cloudwatch_agent_redhat
-# elif [[ -f /etc/lsb-release ]]; then
-#   install_common_packages
-#   download_appjs
-#   configure_nodejs_npm_ubuntu
-#   install_nodejs_packages
-#   install_configure_cloudwatch_agent_ubuntu
-# elif [[ -f /etc/system-release && $(cat /etc/system-release) == "Amazon Linux" ]]; then
-#   configure_nodejs_npm_amazon
-#   install_nodejs_packages
-#   install_configure_cloudwatch_agent_amazon
-# elif [[ -f /etc/centos-release ]]; then
-#   install_common_packages
-#   download_appjs
-#   configure_nodejs_npm_centos
-#   install_nodejs_packages
-#   install_configure_cloudwatch_agent_centos
-# fi
-
-# start_pm2
-
-
-
-
-
-# # #!/bin/bash
-
-# # install_common_packages() {
-# #   echo "Installing common packages..."
-# # }
-
-# # configure_nodejs_npm_redhat() {
-# #   echo "Configuring Node.js and npm for Red Hat..."
-# #   sudo yum install -y nodejs npm
-# #   mkdir ~/.npm-global
-# #   npm config set prefix '~/.npm-global'
-# #   export PATH=~/.npm-global/bin:$PATH
-# #   sudo npm install -g npm@10.2.5
-# # }
-
-# # configure_nodejs_npm_ubuntu() {
-# #   echo "Configuring Node.js and npm for Ubuntu..."
-# #   sudo apt-get update && sudo apt-get install -y nodejs npm
-# #   mkdir ~/.npm-global
-# #   npm config set prefix '~/.npm-global'
-# #   export PATH=~/.npm-global/bin:$PATH
-# #   sudo npm install -g npm@10.2.5
-# # }
-
-# # configure_nodejs_npm_amazon() {
-# #   echo "Configuring Node.js and npm for Amazon Linux..."
-# #   sudo yum install -y nodejs npm
-# #   mkdir ~/.npm-global
-# #   npm config set prefix '~/.npm-global'
-# #   export PATH=~/.npm-global/bin:$PATH
-# #   sudo npm install -g npm@10.2.5
-# # }
-
-# # configure_nodejs_npm_centos() {
-# #   echo "Configuring Node.js and npm for CentOS..."
-# #   sudo yum install -y epel-release
-# #   sudo yum install -y nodejs npm
-# #   mkdir ~/.npm-global
-# #   npm config set prefix '~/.npm-global'
-# #   export PATH=~/.npm-global/bin:$PATH
-# #   sudo npm install -g npm@10.2.5
-# # }
-
-# # install_nodejs_packages() {
-# #   echo "Installing Node.js packages..."
-# #   npm install express body-parser aws-sdk
-# #   npm install -g pm2
-# #   npm install winston winston-cloudwatch @aws-sdk/client-dynamodb
-# # }
-
-# # install_configure_cloudwatch_agent_redhat() {
-# #   echo "Installing and configuring Amazon CloudWatch Agent for Red Hat..."
-# #   sudo yum install -y amazon-cloudwatch-agent
-# #   sudo systemctl start amazon-cloudwatch-agent
-# #   sudo systemctl enable amazon-cloudwatch-agent
-# # }
-
-# # install_configure_cloudwatch_agent_ubuntu() {
-# #   echo "Installing and configuring Amazon CloudWatch Agent for Ubuntu..."
-# #   sudo apt-get install -y amazon-cloudwatch-agent
-# #   sudo systemctl start amazon-cloudwatch-agent
-# #   sudo systemctl enable amazon-cloudwatch-agent
-# # }
-
-# # install_configure_cloudwatch_agent_amazon() {
-# #   echo "Installing and configuring Amazon CloudWatch Agent for Amazon Linux..."
-# #   sudo yum install -y amazon-cloudwatch-agent
-# #   sudo systemctl start amazon-cloudwatch-agent
-# #   sudo systemctl enable amazon-cloudwatch-agent
-# # }
-
-# # install_configure_cloudwatch_agent_centos() {
-# #   echo "Installing and configuring Amazon CloudWatch Agent for CentOS..."
-# #   sudo yum install -y amazon-cloudwatch-agent
-# #   sudo systemctl start amazon-cloudwatch-agent
-# #   sudo systemctl enable amazon-cloudwatch-agent
-# # }
-
-# # start_pm2() {
-# #   echo "Starting app with pm2..."
-# #   pm2 start app.js
-# # }
-
-# # # Check the Linux distribution and run commands accordingly
-# # if [[ -f /etc/redhat-release ]]; then
-# #   install_common_packages
-# #   configure_nodejs_npm_redhat
-# #   install_nodejs_packages
-# #   install_configure_cloudwatch_agent_redhat
-# # elif [[ -f /etc/lsb-release ]]; then
-# #   install_common_packages
-# #   configure_nodejs_npm_ubuntu
-# #   install_nodejs_packages
-# #   install_configure_cloudwatch_agent_ubuntu
-# # elif [[ -f /etc/system-release && $(cat /etc/system-release) == "Amazon Linux" ]]; then
-# #   configure_nodejs_npm_amazon
-# #   install_nodejs_packages
-# #   install_configure_cloudwatch_agent_amazon
-# # elif [[ -f /etc/centos-release ]]; then
-# #   install_common_packages
-# #   configure_nodejs_npm_centos
-# #   install_nodejs_packages
-# #   install_configure_cloudwatch_agent_centos
-# # fi
-
-# # start_pm2
+# Start the application using pm2, checking for package installation
+if command -v pm2 >/dev/null && [[ -f node_modules ]]; then 
+  echo "Starting app.js using pm2..."
+  pm2 start app.js
+else
+  echo "Installing Node.js packages and pm2..."
+  install_nodejs_packages  # Install the packages (including pm2)
+  pm2 start app.js
+fi
